a74861edb571c115d5dc7231ae3d23741bb586c9
YunWooJin <satoru31572@gmail.com>
2025-11-18 17:46:06 +0900

    데이터베이스 연동으로 웹사이트 구현 연습

---
+++ 15.shopping-mall/src/data/ProductList.js
@@ -0,0 +1,76 @@
+let pList = [
+  {
+    id : 1,
+    title : "vest",
+    content : "Made in China",
+    price : 14000
+  },
+  {
+    id : 2,
+    title : "t-shirt",
+    content : "Made in Italy",
+    price : 21000
+  },
+  {
+    id : 3,
+    title : "cotton shirt",
+    content : "Made in Korea",
+    price : 27000
+  },
+  {
+    id : 4,
+    title : "school uniform",
+    content : "Made in Japan",
+    price : 32000
+  },
+  {
+    id : 5,
+    title : "jacket",
+    content : "Made in Japan",
+    price : 41000
+  },
+  {
+    id : 6,
+    title : "sweater",
+    content : "Made in China",
+    price : 22000
+  },
+  {
+    id : 7,
+    title : "collar shirt",
+    content : "Made in USA",
+    price : 26000
+  },
+  {
+    id : 8,
+    title : "dress",
+    content : "Made in Italy",
+    price : 34000
+  },
+  {
+    id : 9,
+    title : "jacket",
+    content : "Made in China",
+    price : 31000
+  },
+  {
+    id : 10,
+    title : "dress",
+    content : "Made in France",
+    price : 29000
+  },
+  {
+    id : 11,
+    title : "collar shirt",
+    content : "Made in Korea",
+    price : 33000
+  },
+  {
+    id : 12,
+    title : "jacket",
+    content : "Made in Japan",
+    price : 36000
+  }
+]
+
+export default pList;
---
+++ 15.shopping-mall/src/pages/Cart.js
@@ -0,0 +1,48 @@
+import {Table} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect,useState } from 'react';
+import axios from 'axios';
+
+function Cart() { 
+    const [list, setList] = useState([]);
+    
+    useEffect(() => {
+        axios.get('/react/getCart')
+             .then(res => {
+                setList(res.data);
+             })
+             .catch(() => {
+                console.log("실패");
+             })
+    }, []);
+
+    return (
+        <div className='cart'>
+            <br/>
+            <h2>CART LIST</h2>
+            <br/>
+
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>번호</th>
+                        <th>상품명</th>
+                        <th>수량</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        list.map(c =>
+                            <tr>
+                                <td>{c.id}</td>
+                                <td>{c.title}</td>
+                                <td>{c.count}</td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </Table>
+        </div>
+    )
+}
+export default Cart;
